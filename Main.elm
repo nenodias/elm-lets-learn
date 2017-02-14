a3cad9adafeacbe18123c0ad00b1c8c58bc0c3ff
Horácio Dias <horacio.dias@yahoo.com>
2017-02-14 00:08:40 -0200

    Atualizando o projeto agora os comentários estão sendo adicionados

---
+++ Main.elm
@@ -0,0 +1,143 @@
+module Main exposing (..)
+import Html exposing (br, button, div, form, h1, input, li, strong, text, textarea, ul)
+--import Html.Attributes exposing ( type_)
+import Html.Events exposing (onInput, onSubmit)
+
+{-
+    Comentário de várias linhas
+    3 Comentários
+
+    Fulano
+    Mensagem do Fulano
+
+    Ciclano
+    Mensagem do Ciclano
+
+    |Formulário|
+    Nome:________
+    Comentário:_________
+    ENVIAR
+-}
+
+--
+-- Model
+--
+type alias Comment =
+    { author: String
+    , content: String
+    }
+
+type alias Model =
+    { comments : List Comment
+    , form : Comment
+    }
+
+initialModel : Model
+initialModel = 
+    Model [] ( Comment "" "" )
+
+
+
+comment1 :Comment
+comment1 = 
+    Comment "Nenodias" "Ahoy"
+
+
+comment2 :Comment
+comment2 = 
+    Comment "Xubiru" "Xuooooo!!!11"
+
+
+altInitialModel : Model
+altInitialModel = 
+    Model [comment1, comment2] ( Comment "" "" )
+
+
+{- Usando Records
+
+initialModel : Model
+initialModel = 
+    { comments = []
+    , form = { author = "", content = "" }
+    }
+
+-}
+
+--
+-- Update
+--
+
+type Msg 
+    = PostComment
+    | UpdateComment String
+    | UpdateAuthor String
+
+update : Msg -> Model -> Model
+update msg model = 
+    case msg of 
+        PostComment ->
+            let
+                comments =
+                 List.append model.comments [ model.form ]
+            in
+            { model | comments = comments }
+        UpdateAuthor value ->
+            { model | form = Comment value model.form.content }
+        UpdateComment value ->
+            { model | form = Comment model.form.author value }
+
+--
+-- View
+--
+
+pluralize : String -> Int -> String
+pluralize nome count =
+    if count == 1 then
+        nome
+    else
+        nome ++ "s"
+
+viewComment : Comment -> Html.Html msg
+viewComment comment =
+    li
+        []
+        [ strong [] [ text comment.author] 
+        , br [] []
+        , text comment.content
+        ]
+
+view : Model -> Html.Html Msg
+view model =
+    let 
+        count = List.length model.comments
+        title = 
+            (toString count) ++ ( pluralize " Comentário" count )
+    in
+    div
+        []
+        [ h1 [][ text title ] 
+        , ul []
+             ( List.map viewComment model.comments )
+        , form
+            [ onSubmit PostComment ]
+            [ text "Nome:"
+            , br [] []
+            , input [ onInput UpdateAuthor ] [] 
+            , br [] []
+            , text "Comentário"
+            , br [] []
+            , textarea [ onInput UpdateComment ] []
+            , br [] []
+            , button [ ] [ text "Enviar"]
+            ]
+        ]
+
+--
+-- Init
+--
+
+main =
+    Html.beginnerProgram
+        { view = view
+        , model = altInitialModel
+        , update = update }
